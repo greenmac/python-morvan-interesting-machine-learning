936729bb6389e05defa28ecf50861650ffaddc0f
greenmac <alwaysmac@msn.com>
2019-03-25 16:57:20 +0800

    machine learning

---
+++ 01-machine-learning-methods/1.1-machine-learing.py
@@ -0,0 +1,7 @@
+# https://morvanzhou.github.io/tutorials/machine-learning/ML-intro/1-1-machine-learning/
+
+# 监督学习 supervised learning;
+# 非监督学习 unsupervised learning;
+# 半监督学习 semi-supervised learning;
+# 强化学习 reinforcement learning;
+# 遗传算法 genetic algorithm.
---
+++ 02-neural-network/2.1-ANN-and-NN.py
@@ -0,0 +1,1 @@
+# https://morvanzhou.github.io/tutorials/machine-learning/ML-intro/2-0-ANN-and-NN/
